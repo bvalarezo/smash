c1a1341ccbec7112429c8bb9602f1a89d1355852
Bryan <bvalarezo@cs.stonybrook.edu>
2020-04-21 23:47:28 -0400

    commit three.sh

--- tests/rsrc/three.sh
+++ tests/rsrc/three.sh
@@ -1,6 +1,6 @@
 #!/home/bvalarezo/hw3-bvalarezo/smash
 echo "I am some of the built-in commands in smash"
-echo "You should see some debug messages in 2.log"
+echo "You should see some debug messages in 3.log"
 sleep 5 &
 kill -19 1
 sleep 60 &
